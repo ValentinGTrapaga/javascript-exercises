2fce7c147d92e3e4080083a8cee4c36c0236cccc
Valentin G Trapaga <gonzaleztrapagav@gmail.com>
2022-04-05 20:27:28 -0300

    realizados ejercicios 1, 2 y 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = ""
+    let strLen = str.length
+    for(let i = strLen - 1; i >= 0; i--) {
+        newStr += str[i]
+    }
+    return newStr
 };
 
 // Do not edit below this line
