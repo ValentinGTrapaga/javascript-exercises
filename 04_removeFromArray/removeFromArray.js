d3d06e9592901f6091d734cc25807c342a48f62b
Valentin G Trapaga <gonzaleztrapagav@gmail.com>
2022-04-06 10:30:04 -0300

    realice ejercicio 4, 5, 6 y 7.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArr = []
+    let arrLength = arr.length
+    let arrToCompare = [...args]
+    for (let i = 0; i < arrLength; i++) {
+        if (!arrToCompare.includes(arr[i])) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
 };
 
 // Do not edit below this line
